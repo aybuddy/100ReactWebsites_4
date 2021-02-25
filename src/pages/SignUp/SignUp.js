a8a2673af0577e2815a86d22b2f8add603bce81b
Aaron Yu <ayybuddyy@gmail.com>
2021-02-25 14:20:26 -0500

    Update routes pages and install scroll to top behavior

---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { homeObjThree } from './Data';
+import { InfoSection, Pricing } from '../../components';
+
+const SignUp = () => {
+  return (
+    <>
+      <InfoSection {...homeObjThree} />
+    </>
+  );
+};
+
+export default SignUp;
