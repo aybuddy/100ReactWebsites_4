1b2b9d3a29201b8dcc3bcfc8b07c3cdb81167635
Aaron Yu <ayybuddyy@gmail.com>
2021-02-25 09:32:28 -0500

    Start creating navbar components, install styled-components, export components to single index file

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Nav, NavbarContainer } from './Navbar.styles';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer></NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
