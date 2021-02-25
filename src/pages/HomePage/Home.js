6291b9cab9cfae39398696a2ed89fbb8104fd896
Aaron Yu <ayybuddyy@gmail.com>
2021-02-25 14:08:19 -0500

    Add in Pricing component

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { homeObjFour, homeObjOne, homeObjThree, homeObjTwo } from './Data';
-import { InfoSection } from '../../components';
+import { InfoSection, Pricing } from '../../components';
 
 const Home = () => {
   return (
@@ -8,6 +8,7 @@ const Home = () => {
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
       <InfoSection {...homeObjThree} />
+      <Pricing />
       <InfoSection {...homeObjFour} />
     </>
   );
